1f3572ab55a2c23a8e3ce7406f62cec6632f9c50
Jônatas Vitalino <jcv@cin.ufpe.br>
2020-01-31 22:22:50 -0300

    Tabuleiro pronto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
